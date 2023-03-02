a5e46bf03f6c5e951a3fb1e5b60aa89ef41b70df
devinvolk@comcast.net <devinvolk@Devins-MacBook-Air.local>
2023-03-01 16:57:51 -0800

    sign up form

--- app/forms.py
+++ app/forms.py
@@ -10,3 +10,10 @@ class LoginForm(FlaskForm):
     email = EmailField('Email:', validators=[DataRequired()])
     password = PasswordField('Password:', validators=[DataRequired()])
     submit_btn = SubmitField('Login')
+
+class SignUp(FlaskForm):
+    first_name = StringField('First Name:')
+    last_name = StringField('Last Name:')
+    email = EmailField('Email:', validators=[DataRequired()])
+    password = PasswordField('Passwrord:', validators=[DataRequired()])
+    submit_btn = SubmitField('Sign Up')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, request
 import requests
-from app.forms import PokeSearch, LoginForm
+from app.forms import PokeSearch, LoginForm, SignUp
 from app import app
 
 @app.route('/', methods=['GET'])
@@ -17,9 +17,15 @@ def login():
             return f'Successfully logged in! Hello, {app.config.get("REGISTERED_USERS").get(email).get("name")}'
         else:
             error = 'Invalid login'
-            return error
-    print('Not validated')
+            return render_template('login.html', error=error, form=form)
     return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['Get', 'POST'])
+def signup():
+    form = SignUp()
+    # if request.method == 'POST' and form.validate_on_submit():
+    return render_template('signup.html', form=form)
+
 
 @app.route('/pokemon', methods=['GET', 'POST'])
 def pokemon():
@@ -44,5 +50,5 @@ def pokemon():
             return render_template('pokemon.html', form=form, pokedex=pokedex)
         else:
             error = 'That Pokemon is not in our database.'    
-            return render_template('pokemon.html', error=error)
+            return render_template('pokemon.html', error=error, form=form)
     return render_template('pokemon.html', form=form)
