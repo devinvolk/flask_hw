28cbfbf31d18966557e8800fb946373b63dde2ac
devinvolk@comcast.net <devinvolk@Devins-MacBook-Air.local>
2023-03-11 20:18:53 -0800

    release functionality built and functional

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -40,17 +40,17 @@ def pokemon():
 @main.route('/catch/<string:name>', methods=['GET', 'POST'])
 @login_required
 def catch(name):
-    print('in catch route')
     queried_pokemon = PokeTable.query.filter_by(pokemon_name=name).first()
     print(queried_pokemon)
         # check if the pokemon is already on the user's team
     if queried_pokemon:
+        #checks to see if the user already has the pokemon on their team
         if current_user.check_team(queried_pokemon):
             #count of user_id is 5, do not add pokemon
             if len(current_user.team.all()) < 5:
                 #link existing pokemon data from PokeTable to the User_id
                 current_user.add_pokemon(queried_pokemon)
-                flash('The Pokemon was added to your team!', 'success')
+                flash(f'The {name.title()} was added to your team!', 'success')
                 return redirect(url_for('main.pokemon'))
             else:    
                 flash(f'Could not add {name.title()} to your team. You already have 5 Pokemon', 'danger')
@@ -59,7 +59,6 @@ def catch(name):
             flash(f'{name.title()} is already on your team', 'danger')
             return redirect(url_for('main.pokemon'))
     else:
-        print('in else')
         url = f'https://pokeapi.co/api/v2/pokemon/{name.lower()}'
         response = requests.get(url)
         pokemon_dict= {
@@ -81,3 +80,18 @@ def catch(name):
         else:    
             flash(f'Could not add {pokemon_dict["Name"].title()} to your team. You already have 5 Pokemon', 'danger')
             return redirect(url_for('main.pokemon'))
+
+@main.route('/team', methods=['GET', 'POST'])
+@login_required
+def team():
+    return render_template('team.html')
+
+@main.route('/release/<string:name>', methods=['GET', 'POST'])
+@login_required
+def release(name):
+    squad = current_user.team
+    for pokemon in squad:
+        if pokemon.pokemon_name == name:
+            current_user.remove_pokemon(pokemon)
+    flash(f'{name} was removed from your team', 'success')
+    return redirect(url_for('main.team'))
